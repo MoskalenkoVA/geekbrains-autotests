85bf614ff088b3506ab1081aa73cb7338d998875
Vladimir <moskalenko_v_a@mail.ru>
2020-12-25 15:26:37 +0300

    version 1.0

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,31 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static WebDriver driver;
+    public static void main( String[] args ) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://google.com");//
+        WebElement input = driver.findElement(By.cssSelector("input[title='Поиск']"));
+        input.click();
+        input.sendKeys("github");
+        input.sendKeys(Keys.ENTER);
+        Thread.sleep(10000);
+        WebElement wikiLink = driver.findElement(By.linkText("Википедия"));
+        String wikiLinkString = wikiLink.getAttribute("href");
+        System.out.println(wikiLinkString);
+        driver.quit();
     }
 }
