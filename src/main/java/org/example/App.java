0678030f8eb2564b86ede24a873d5b668174d6b6
Vladimir <moskalenko_v_a@mail.ru>
2020-12-25 19:48:09 +0300

    dz 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Hello world!
@@ -14,18 +17,80 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App 
 {
     public static WebDriver driver;
-    public static void main( String[] args ) throws InterruptedException {
+    public static final String PASSWORD = "302911AsD";
+
+    public static WebDriver getDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://google.com");//
-        WebElement input = driver.findElement(By.cssSelector("input[title='Поиск']"));
-        input.click();
-        input.sendKeys("github");
-        input.sendKeys(Keys.ENTER);
-        Thread.sleep(10000);
-        WebElement wikiLink = driver.findElement(By.linkText("Википедия"));
-        String wikiLinkString = wikiLink.getAttribute("href");
-        System.out.println(wikiLinkString);
+        return driver;
+    }
+    public static void main( String[] args ) throws InterruptedException {
+        getDriver();
+        test1(driver);
+        test2(driver);
         driver.quit();
+
+
+    }
+
+    public static void test1(WebDriver driver) {
+        driver.get("https://www.diary.ru/");
+        System.out.println("Page title is: " + driver.getTitle());
+        WebElement input = driver.findElement(By.id("drop-login"));
+        input.click();
+        WebElement login = driver.findElement(By.id("usrlog2"));
+        login.click();
+        login.sendKeys("Дино_завр");
+        WebElement password = driver.findElement(By.id("usrpass2"));
+        password.click();
+        password.sendKeys(PASSWORD);
+        WebElement input1 = driver.findElement(By.xpath("//*[@id=\"loginform\"]/form/button"));
+        input1.click();
+        WebElement check1 = driver.findElement(By.linkText("Дино_завр"));
+        new WebDriverWait(driver, 2).until(ExpectedConditions.visibilityOf(check1));
+        WebElement drop = driver.findElement(By.id("drop"));
+        drop.click();
+        WebElement exit = driver.findElement(By.linkText("Выход"));
+        exit.click();
+
+
+    }
+
+    public static void test2(WebDriver driver) {
+        driver.get("https://www.diary.ru");
+        System.out.println("Page title is: " + driver.getTitle());
+        WebElement input = driver.findElement(By.id("drop-login"));
+        input.click();
+        WebElement login = driver.findElement(By.id("usrlog2"));
+        login.click();
+        login.sendKeys("Дино_завр");
+        WebElement password = driver.findElement(By.id("usrpass2"));
+        password.click();
+        password.sendKeys(PASSWORD);
+        WebElement input1 = driver.findElement(By.xpath("//*[@id=\"loginform\"]/form/button"));
+        input1.click();
+        WebElement check1 = driver.findElement(By.linkText("Дино_завр"));
+        new WebDriverWait(driver, 2).until(ExpectedConditions.visibilityOf(check1));
+        WebElement newRecord = driver.findElement(By.xpath("//*[@id=\"body\"]/div[2]/div/ul[2]/li[2]/a"));
+        newRecord.click();
+        WebElement check2 = driver.findElement(By.linkText("Динозаврыч"));
+        new WebDriverWait(driver, 2).until(ExpectedConditions.visibilityOf(check2));
+        WebElement title = driver.findElement(By.id("postTitle"));
+        title.click();
+        title.sendKeys("AutoTestWeb");
+        WebElement msg = driver.findElement(By.xpath("//*[@id=\"message\"]"));
+        msg.click();
+        msg.sendKeys("Auto test№2 OK");
+        WebElement save = driver.findElement(By.id("rewrite"));
+        save.click();
+        WebElement drop = driver.findElement(By.id("drop"));
+        drop.click();
+        WebElement exit = driver.findElement(By.linkText("Выход"));
+        exit.click();
+        driver.quit();
+
+
+
+
     }
 }
